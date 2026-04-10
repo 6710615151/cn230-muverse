843e2dd69d84c3026e55ff3676837f44c301413c
FANNGUB <fanfan.com>
2026-04-10 21:29:11 +0700

    initial

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
- 
+
 app.use(express.json());
- 
+
 app.get("/api/health", (req, res) => {
   res.json({ success: true, status: "ok" });
 });
- 
-// import routes
-app.use("/api/students", require("./student"));
-app.use("/api/courses", require("./courses"));
-app.use("/api/enrollments", require("./enrollments"));
-app.use("/api/grades", require("./grades"));
-app.use("/api/report", require("./report"));
-app.use("/api/schema", require("./schema"));
- 
-module.exports = app;
 
+// import routes
+import student from "./student.js";
+import courses from "./courses.js";
+import enrollments from "./enrollments.js";
+import grades from "./grades.js";
+import report from "./report.js";
+import schema from "./schema.js";
+
+app.use("/api/students", student);
+app.use("/api/courses", courses);
+app.use("/api/enrollments", enrollments);
+app.use("/api/grades", grades);
+app.use("/api/report", report);
+app.use("/api/schema", schema);
+
+export default app;
